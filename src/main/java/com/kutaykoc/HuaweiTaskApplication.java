0b630d306572245f7f479fb696ff5f77d5e49942
kutay <kutaykoc@outlook.com>
2018-12-12 10:00:17 +0300

    dasdsad

--- src/main/java/com/kutaykoc/HuaweiTaskApplication.java
+++ src/main/java/com/kutaykoc/HuaweiTaskApplication.java
@@ -2,11 +2,24 @@ package com.kutaykoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HuaweiTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HuaweiTaskApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
--- src/main/java/com/kutaykoc/WebSecurityConfig.java
+++ src/main/java/com/kutaykoc/WebSecurityConfig.java
@@ -6,4 +6,5 @@ package com.kutaykoc;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebSecurityConfig {
+
 }
